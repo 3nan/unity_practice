1faa3938cb115a2288ab1f4d904084d881b2ff81
江田光義 <3nan@koda.local>
2016-02-09 09:03:57 +0900

    add success project

---
+++ sampleblock/Assets/Scene/Rankingsample.cs
@@ -0,0 +1,32 @@
+﻿using Kayac.Lobi.SDK;
+using UnityEngine;
+using System.Collections;
+
+public class Rankingsample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ButtonPush() {
+		//Debug.Log("Button Push !!");
+		if (LobiCoreBridge.IsSignedIn()) {
+			Debug.Log("アカウント作成済み");
+			/* アカウントが作成済みかチェック */
+			int val = (int) (10000.0f*Random.value);
+			LobiRankingAPIBridge.SendRanking("Button", "SendRankingCallback", "test01", val);
+		}else{
+			Debug.Log("アカウント作成がまだ");
+			LobiCoreAPIBridge.SignupWithBaseName("Button","SendAccountCallBack", "PlayerName");
+		}
+	}
+
+	void SendRankingCallback(){}
+	void SendAccountCallBack(string message){Debug.Log (message);}
+}
